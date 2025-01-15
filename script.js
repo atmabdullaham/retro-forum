740a5491cd8290674983f9f3d56a3bf7eda4e96b
atmabdullaham <atmabdullaham@gmail.com>
2025-01-15 12:39:13 +0600

    show dynamically the latest post

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ const loadAllPosts = async (category) => {
 
 const displayAllPost = (posts) => {
  const postContainer = document.getElementById("post-container");
+ postContainer.innerHTML = "";
  posts.forEach(post => {
-  console.log(post)
+
   const div = document.createElement("div")
   div.innerHTML = `
    <div
@@ -101,4 +102,54 @@ loadAllPosts();
 const handleSearchByCategory = () => {
  const searchTexts = document.getElementById("searchPosts").value;
  loadAllPosts(searchTexts)
+}
+
+const loadLatestPost = () => {
+ fetch("https://openapi.programming-hero.com/api/retro-forum/latest-posts")
+  .then(res => res.json())
+  .then(data => showLatestPost(data))
+  .catch(error => console.log(error))
+}
+loadLatestPost();
+
+const showLatestPost = (array) => {
+ const latestPostContainer = document.getElementById("latest-post-container");
+ array.forEach(post => {
+  const div = document.createElement("div")
+  div.innerHTML = `
+  <div class="card lg:w-96 pb-5 bg-base-100 shadow-2xl">
+             <figure class="lg:px-6 px-4 pt-4 lg:pt-8">
+               <img
+                  src=${post.cover_image}
+                  alt="Shoes"
+                  class="rounded-xl"
+              />
+             </figure>
+           <div class="p-5 lg:p-10 space-y-4 lg:space-y-5">
+               <p class="opacity-50 text-start">
+                   <i class="fa-solid fa-calendar-days me-2"></i>${post.author.posted_date ? `${post.author.posted_date}` : "No Publish Date"}
+               </p>
+               <h2 class="card-title text-start">${post.title}</h2>
+               <p class="text-start">
+                  ${post.description}
+               </p>
+               <div class="card-actions flex gap-5 items-center">
+                   <div class="avatar">
+                       <div
+                          class="lg:w-12 w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2"
+                      >
+                          <img
+                          src=${post.profile_image}
+                          />
+                      </div>
+                  </div>
+               <div>
+                <h3 class="text-start font-extrabold">${post.author.name}</h3>
+                <p class="text-start opacity-60">${post.author.designation ? `${post.author.designation}` : "Unknown"}</p>
+               </div>
+           </div>
+  `
+  latestPostContainer.appendChild(div);
+  console.log(post)
+ });
 }
