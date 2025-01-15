580778e87d1d66848ccb4dfaaa4253556a46f8b2
atmabdullaham <atmabdullaham@gmail.com>
2025-01-15 09:36:04 +0600

    load all posts

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("connected");
+const loadAllPosts = async () => {
+ const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`)
+ const data = await res.json();
+ console.log(data.posts);
+}
+loadAllPosts();
