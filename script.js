ed8512b40ec80bf5efd0f49da62b868b988ff1d9
atmabdullaham <atmabdullaham@gmail.com>
2025-01-15 10:58:27 +0600

    adde the dynamic card for showing all data

--- script.js
+++ script.js
@@ -8,7 +8,65 @@ const loadAllPosts = async (category) => {
 }
 
 const displayAllPost = (posts) => {
- console.log(posts)
+ const postContainer = document.getElementById("post-container");
+ posts.forEach(post => {
+  console.log(post)
+  const div = document.createElement("div")
+  div.innerHTML = `
+   <div
+  class ="p-6 lg:p-12 flex gap-6 lg:flex-row flex-col items-center lg: items-start bg-[#F3F3F5] rounded-3xl">
+  <div class = "indicator">
+    <span class = 'indicator-item badge ${post.isActive ? "bg-green-600" : "bg-red-500"}' > </span>
+    
+    <div class ="avatar" > 
+      <div class = "w-24 rounded-xl">
+      <img 
+       src = ${post.image}
+      />
+      </div>
+    </div> 
+    </div>
+
+    <div class ="space-y-4 w-full" > 
+      <div class ="flex gap-4 *:opacity-60" >
+      <p>#${post.category}</p>
+      <p>Author: ${post.author.name}</p>
+      </div>
+      <h3 class = text-2xl font-bold opacity-70 >
+       ${post.title}
+      </h3>
+      <p class = "opacity-40" > 
+        ${post.description}
+      </p>
+      <hr class = "border border-dashed border-gray-300" />
+      <div class = "flex justify-between *:font-bold [&>*:not(:last-child)]:opacity-45" >
+      <div class = "space-x-2 flex items-center">
+        <i class = "fa-regular fa-comment-dots"></i>
+        <p>${post.comment_count}</p>
+      </div>
+       <div class = "space-x-2 flex items-center">
+        <i class = "fa-regular fa-eye"></i>
+        <p>${post.view_count}</p>
+      </div>
+      <div class = "space-x-2 flex items-center">
+        <i class = "fa-regular fa-clock"></i>
+        <p>${post.posted_time} Min</p>
+      </div>
+     
+<div class = "opacity-100">
+<button id ="addToList"
+data-post='${JSON.stringify(post)}' class = "addToList btn btn-circle bg-green-500 btn-sm">
+<i class = "fa-solid fa-envelope-open text-white"></i>
+ </button>
+    </div>
+    </div>
+    </div>
+ </div>
+    
+
+ `
+  postContainer.appendChild(div);
+ });
 }
 
 
