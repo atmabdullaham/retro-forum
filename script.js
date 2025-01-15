4dc77a6a74ac75f3bf8e532238b424f99a01e6a2
atmabdullaham <atmabdullaham@gmail.com>
2025-01-15 10:02:15 +0600

    load all post and search post dynamically

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const loadAllPosts = async () => {
- const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`)
+const loadAllPosts = async (category) => {
+
+ // console.log(`https://openapi.programming-hero.com/api/retro-forum/posts${category ? `?category=${category}` : ''}`);
+
+ const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts${category ? `?category=${category}` : ''}`)
  const data = await res.json();
- console.log(data.posts);
+ displayAllPost(data.posts);
 }
+
+const displayAllPost = (posts) => {
+ console.log(posts)
+}
+
+
 loadAllPosts();
+const handleSearchByCategory = () => {
+ const searchTexts = document.getElementById("searchPosts").value;
+ loadAllPosts(searchTexts)
+}
